3cde4384a7c6d91c8bf05ee7fd07c8c7f2bea307
Annie Liao <liaowow@gmail.com>
2020-04-13 15:26:37 -0400

    updated code on event handling and added readme

--- README.md
+++ README.md
@@ -3,4 +3,12 @@
 ### v-bind
 Dynamically binds an attribute to an expression.
 
- 
+### v-show
+Dynamically displays/hides DOM element based on boolean value
+
+### v-for
+Loops over the array and displays each element inside the array
+
+### v-on
+Event Listener
+Short-hand version: @
--- main.js
+++ main.js
@@ -6,6 +6,33 @@ var app = new Vue({
         product: "Socks",
         image: './assets/vmSocks-green.jpg',
         inventory: 8,
-        onSale: false
+        onSale: false,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: "green",
+                variantImage: './assets/vmSocks-green.jpg'         
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue",
+                variantImage: './assets/vmSocks-blue.jpg'
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function() {
+            this.cart += 1
+        },
+
+        remove1FromCart: function() {
+            this.cart -= 1
+        },
+
+        updateProduct: function(variantImage) {
+            this.image = variantImage
+        }
     }
 })
