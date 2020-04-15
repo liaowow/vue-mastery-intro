620750248167c3573f649832d7a1ccbb0fe8d891
Annie Liao <liaowow@gmail.com>
2020-04-15 10:47:45 -0400

    added code practice on computed properties

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ Loops over the array and displays each element inside the array.
 
 ### v-on
 Event Listener. (Short-hand version: @)
+
+### Computed Properties
+Acting as a calculator, computed properties are cached, meaning the results are saved until the dependencies change. It makes the operation more perfomant than a method, which can be more expensive in terms of operation (we don't want to re-run whenever we access it)
--- main.js
+++ main.js
@@ -3,22 +3,26 @@ var product = "Socks"
 var app = new Vue({
     el: '#app',
     data: {
+        brand: "Vue Mastery",
         product: "Socks",
-        image: './assets/vmSocks-green.jpg',
-        inventory: 100,
-        inStock: true,
-        onSale: false,
+        selectedVariant: 0,
+        // image: './assets/vmSocks-green.jpg',
+        // inventory: 100,
+        // inStock: true,
+        onSale: true,
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         variants: [
             {
                 variantId: 2234,
                 variantColor: "green",
-                variantImage: './assets/vmSocks-green.jpg'         
+                variantImage: './assets/vmSocks-green.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: "blue",
-                variantImage: './assets/vmSocks-blue.jpg'
+                variantImage: './assets/vmSocks-blue.jpg',
+                variantQuantity: 0
             }
         ],
         cart: 0
@@ -32,8 +36,27 @@ var app = new Vue({
             this.cart -= 1
         },
 
-        updateProduct: function(variantImage) {
-            this.image = variantImage
+        updateProduct: function(index) {
+            this.selectedVariant = index
+            console.log(index)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        displaySale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' are on sale!'
+            }
+
+            return this.title()
         }
     }
 })
